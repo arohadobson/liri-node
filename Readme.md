969048757b32a49554b2d43b97ebc618d20e5ed3
Aroha Dobson <aroha.dobson@arohamarieprogramming.com>
2018-12-13 00:29:26 -0700

    updated Readme.md

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# LIRI
+
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. All previous queries are saved within the log.txt file.
+
+## How To Search
+
+LIRI is a command line node app that can search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+## Code Languages
+
+    • JavaScript
+    • Node.js
+    • npm
+
+## Gifs
+
+Running command 'spotify-this-song' "[song choice]'
+![LIRI Runthrough 'spotify-this-song'](https://media.giphy.com/media/1fovMhjmPncViF3UrT/giphy.gif)
+
+Running command 'movie-this' "[movie choice]'
+![LIRI Runthrough ](https://media.giphy.com/media/WS39sYde9wLTtSz9UR/giphy.gif)
+
+Running command 'do-what-it-says': this will automatically return pre-loaded information listed in the random.text file.
+![LIRI Runthrough ](https://media.giphy.com/media/WS39sYde9wLTtSz9UR/giphy.gif)
--- liri.js
+++ liri.js
@@ -1,109 +1,131 @@
-var divider = "\n----------------------------------\n"
+var divider = "\n----------------------------------\n";
 
 //Spotify Function
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 
-var spotify = new Spotify ({
-	id: 'df16d8312ed54b45b4d65dc32a81d78d',
-	secret: '176efb35dedf483ebb4371809d847fdd'
+var spotify = new Spotify({
+  id: "df16d8312ed54b45b4d65dc32a81d78d",
+  secret: "176efb35dedf483ebb4371809d847fdd"
 });
 
-function spotifyWrite () {
+function spotifyWrite() {
+  var queryTerm;
+  var comboTerm = "";
 
-	var queryTerm;
-	var comboTerm = "";
+  for (var i = 3; i < process.argv.length; i++) {
+    comboTerm += process.argv[i] + " ";
+  }
 
-	for (var i = 3; i < process.argv.length; i++) {
-		comboTerm += process.argv[i] + " ";
-	}
+  if (process.argv[2] == "spotify-this-song" && process.argv[3] != undefined) {
+    queryTerm = comboTerm;
+  } else if (
+    process.argv[2] == "spotify-this-song" &&
+    process.argv[3] == undefined
+  ) {
+    queryTerm = "The Sign, Ace of Base";
+  } else if (process.argv[2] == "do-what-it-says") {
+    queryTerm = readQuery;
+  }
 
-	if (process.argv[2] == "spotify-this-song" && process.argv[3] != undefined) {
-		queryTerm = comboTerm;
-	}
-	else if (process.argv[2] == "spotify-this-song" && process.argv[3] == undefined) {
-		queryTerm = "The Sign, Ace of Base";
-	}
-	else if (process.argv[2] == "do-what-it-says") {
-		queryTerm = readQuery;
-	}
+  spotify.search({ type: "track", query: queryTerm, limit: 1 }, function(
+    err,
+    data
+  ) {
+    if (data.tracks.items[0] == undefined) {
+      console.log("No Song Found");
+      return;
+    }
 
-	spotify.search({type: 'track', query: queryTerm, limit: 1}, function (err, data) {
+    var artist = data.tracks.items[0].artists[0].name;
 
-		if (data.tracks.items[0] == undefined) {
-			console.log("No Song Found");
-			return;
-		}
+    for (var i = 1; i < data.tracks.items[0].artists.length; i++) {
+      artist += ", " + data.tracks.items[0].artists[i].name;
+    }
 
-		var artist = data.tracks.items[0].artists[0].name;
+    // console.log(artist);
 
-		for (var i = 1; i < data.tracks.items[0].artists.length; i++) {
-			artist += ", " + data.tracks.items[0].artists[i].name;
-		}
+    var songMessage =
+      "Song: " +
+      data.tracks.items[0].name +
+      "\nArtist: " +
+      artist +
+      "\nAlbum: " +
+      data.tracks.items[0].album.name +
+      "\nLink: " +
+      data.tracks.items[0].external_urls.spotify;
 
-		// console.log(artist);
+    songMessage += divider;
 
-		var songMessage = "Song: " + data.tracks.items[0].name + "\nArtist: " + artist + "\nAlbum: " + data.tracks.items[0].album.name + "\nLink: " + data.tracks.items[0].external_urls.spotify;
+    console.log(divider + songMessage);
 
-		songMessage += divider;
-
-		console.log(divider + songMessage);
-
-		fs.appendFile("log.txt", songMessage, function (err) {
-			if (err) {
-				console.log(err);
-			}
-		})
-	})
+    fs.appendFile("log.txt", songMessage, function(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
 }
 
 // spotifyWrite();
 
 //OMDB Function
-function movieWrite () {
-	var request = require('request');
+function movieWrite() {
+  var request = require("request");
 
-	var queryTerm;
-	var comboTerm = "";
+  var queryTerm;
+  var comboTerm = "";
 
-	for (var i = 3; i < process.argv.length; i++) {
-		comboTerm += process.argv[i] + " ";
-	}
+  for (var i = 3; i < process.argv.length; i++) {
+    comboTerm += process.argv[i] + " ";
+  }
 
-	if (process.argv[2] == "movie-this" && process.argv[3] != undefined) {
-		queryTerm = comboTerm;
-	}
-	else if (process.argv[2] == "movie-this" && process.argv[3] == undefined) {
-		queryTerm = "Mr. Nobody";
-	}
-	else if (process.argv[2] == "do-what-it-says") {
-		queryTerm = readQuery;
-	}
+  if (process.argv[2] == "movie-this" && process.argv[3] != undefined) {
+    queryTerm = comboTerm;
+  } else if (process.argv[2] == "movie-this" && process.argv[3] == undefined) {
+    queryTerm = "Mr. Nobody";
+  } else if (process.argv[2] == "do-what-it-says") {
+    queryTerm = readQuery;
+  }
 
-	var queryURL = "http://www.omdbapi.com/?t=" + queryTerm + "&apikey=40e9cece"
+  var queryURL = "http://www.omdbapi.com/?t=" + queryTerm + "&apikey=40e9cece";
 
-	request(queryURL, function (error, response, body) {
-		var obj = JSON.parse(body);
+  request(queryURL, function(error, response, body) {
+    var obj = JSON.parse(body);
 
-		if (error) {
-			return console.log(error);
-		}
-		else if (obj.Response == "False") {
-			return console.log("No Movie Found");
-		}
+    if (error) {
+      return console.log(error);
+    } else if (obj.Response == "False") {
+      return console.log("No Movie Found");
+    }
 
-		var movieMessage = "Title: " + obj.Title + "\nYear: " + obj.Year + "\nIMDB Rating: " + obj.Ratings[0].Value + "\nRotten Tomatoes Rating: " + obj.Ratings[1].Value + "\nCountry: " + obj.Country + "\nLanguage: " + obj.Language + "\nPlot: " + obj.Plot + "\nActors: " + obj.Actors;
+    var movieMessage =
+      "Title: " +
+      obj.Title +
+      "\nYear: " +
+      obj.Year +
+      "\nIMDB Rating: " +
+      obj.Ratings[0].Value +
+      "\nRotten Tomatoes Rating: " +
+      obj.Ratings[1].Value +
+      "\nCountry: " +
+      obj.Country +
+      "\nLanguage: " +
+      obj.Language +
+      "\nPlot: " +
+      obj.Plot +
+      "\nActors: " +
+      obj.Actors;
 
-		movieMessage += divider;
+    movieMessage += divider;
 
-		console.log(divider + movieMessage);
+    console.log(divider + movieMessage);
 
-		fs.appendFile("log.txt", movieMessage, function (err) {
-			if (err) {
-				console.log(err);
-			}
-		})
-
-	})
+    fs.appendFile("log.txt", movieMessage, function(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
 }
 
 // movieWrite();
@@ -112,27 +134,22 @@ function movieWrite () {
 var readQuery;
 var fs = require("fs");
 
-function doThisWrite () {
+function doThisWrite() {
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
 
-	fs.readFile("random.txt", "utf8", function (err, data) {
+    data = data.split(", ");
 
-		if (err) {
-			return console.log(err);
-		}
+    readQuery = data[1];
 
-		data = data.split(", ")
-
-		readQuery = data[1];
-
-	 if (data[0] == "spotify-this-song") {
-		 	spotifyWrite();
-		}
-		else if (data[0] == "movie-this") {
-		    movieWrite();
-		}
-
-	})
-
+    if (data[0] == "spotify-this-song") {
+      spotifyWrite();
+    } else if (data[0] == "movie-this") {
+      movieWrite();
+    }
+  });
 }
 
 // doThisWrite ();
@@ -140,18 +157,20 @@ function doThisWrite () {
 var action = process.argv[2];
 
 switch (action) {
-	case "spotify-this-song":
-		spotifyWrite();
-		break;
+  case "spotify-this-song":
+    spotifyWrite();
+    break;
 
-	case "movie-this":
-		movieWrite();
-		break;
+  case "movie-this":
+    movieWrite();
+    break;
 
-	case "do-what-it-says":
-		doThisWrite();
-		break;
-	default:
-		console.log("Please Use a Working Command: \nspotify-this-song \nmovie-this \ndo-what-it-says");
-		break;
+  case "do-what-it-says":
+    doThisWrite();
+    break;
+  default:
+    console.log(
+      "Please Use a Working Command: \nspotify-this-song \nmovie-this \ndo-what-it-says"
+    );
+    break;
 }
